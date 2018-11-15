8894a0f28f75b3bb77f83af763962f29a814974a
“hiepdinhnguyenhoang” <hiepdinhnguyenhoang@gmail.com>
2018-11-15 11:07:56 +0700

    aaaaa

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,16 +1,40 @@
 class StaticPagesController < ApplicationController
   def home
     if logged_in?
+
+
+
+
+
+
+
       @micropost = current_user.microposts.build
+
+
+
+
+
+
+
       @feed_items = current_user.feed.paginate page: params[:page]
     end
   end
 
-  
 
+
+
+
+
+
+  
+    
 
   def hello
   	puts "hello world"
+
+
+
+\
   	
   end
 end
